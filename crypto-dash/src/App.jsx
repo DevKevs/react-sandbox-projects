909b8dd952e4504021767fcedb5c94be00ad25c0
Kevin Féliz <75509889+DevKevs@users.noreply.github.com>
2025-08-11 16:20:31 -0400

    CryptoDash V1

---
+++ crypto-dash/src/App.jsx
@@ -0,0 +1,9 @@
+import CoinsContainer from "./Components/CoinsContainer";
+
+const App = () => {
+  return (
+    <CoinsContainer />
+  );
+};
+
+export default App;
---
+++ crypto-dash/src/Components/CoinsContainer.jsx
@@ -0,0 +1,89 @@
+import { useState, useEffect } from "react";
+import CoinCard from "./CoinCard";
+import LimitSelector from "./Filters/LimitSelector";
+import FilterInput from "./Filters/FilterInput";
+import SortSelector from "./Filters/SortSelector";
+const API_URL = import.meta.env.VITE_API_URL;
+
+const CoinsContainer = () => {
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [limit, setLimit] = useState(10);
+  const [filter, setFilter] = useState("");
+  const [sortBy, setSortBy] = useState("market_cap_desc");
+
+  useEffect(() => {
+    const fetchCoins = async () => {
+      try {
+        const res = await fetch(
+          `${API_URL}&order=market_cap_desc&per_page=${limit}&page=1&sparkline=false`
+        );
+        if (!res.ok) throw new Error("Network response was not ok");
+        const data = await res.json();
+        console.log(data);
+        setCoins(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCoins();
+  }, [limit]);
+
+  const filteredCoins = coins
+    .filter((coin) => {
+      return (
+        coin.name.toLowerCase().includes(filter.toLowerCase()) ||
+        coin.symbol.toLowerCase().includes(filter.toLowerCase())
+      );
+    })
+    .slice()
+    .sort((a, b) => {
+      switch (sortBy) {
+        case "market_cap_desc":
+          return b.market_cap - a.market_cap;
+        case "market_cap_asc":
+          return a.market_cap - b.market_cap;
+        case "price_desc":
+          return b.current_price - a.current_price;
+        case "price_asc":
+          return a.current_price - b.current_price;
+        case "change_desc":
+          return b.price_change_percentage_24h - a.price_change_percentage_24h;
+        case "change_asc":
+          return a.price_change_percentage_24h - b.price_change_percentage_24h;
+        default:
+          return 0;
+      }
+    });
+
+  return (
+    <div>
+      <h1>🚀 Crypto Dash</h1>
+      <div className="top-controls">
+        <FilterInput filter={filter} onFilterChange={setFilter} />
+        <LimitSelector limit={limit} onLimitChange={setLimit} />
+        <SortSelector sortBy={sortBy} onSortChange={setSortBy} />
+      </div>
+
+      {loading && <p>Loading...</p>}
+
+      {error && <div className="error">Error: {error}</div>}
+
+      {!loading && !error && (
+        <main className="grid">
+          {filteredCoins.length > 0 ? (
+            filteredCoins.map((coin) => <CoinCard key={coin.id} coin={coin} />)
+          ) : (
+            <p>No coins found</p>
+          )}
+        </main>
+      )}
+    </div>
+  );
+};
+
+export default CoinsContainer;
